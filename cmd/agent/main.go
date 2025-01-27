bbddca239e822635547433db12470391b0a1d6c5
Morzisorn <morzisorc1@gmail.com>
2025-01-27 19:42:16 +0000

    Iter2. Add agent and unit tests

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+
+	"github.com/morzisorn/metrics/internal/agent"
+)
+
+const (
+	pollInterval   = 2
+	reportInterval = 10
+)
+
+func main() {
+	now := time.Now()
+	lastReport := time.Now()
+	m := agent.Metrics{}
+	for {
+		if time.Since(now).Seconds() >= pollInterval {
+			now = time.Now()
+			err := m.PollAllMetrics()
+			if err != nil {
+				fmt.Println(err)
+			}
+			if time.Since(lastReport) >= reportInterval {
+				lastReport = time.Now()
+				err := m.SendAllMetrics()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,60 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/morzisorn/metrics/internal/storage"
+	server "github.com/morzisorn/metrics/internal/server/handlers"
 )
-
-var s = storage.MemStorage{
-	Metrics: make(map[string]float64),
-}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
 }
 
-func update(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost || r.Header.Get("Content-Type") != "text/plain" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-
-	splitPath := strings.Split(r.URL.Path, "/")
-	if len(splitPath) != 5 {
-		http.Error(w, "Invalid path", http.StatusBadRequest)
-	}
-
-	typeMetric := splitPath[2]
-	nameMetric := splitPath[3]
-	valueMetric := splitPath[4]
-
-	if nameMetric == "" {
-		http.Error(w, "Invalid name or value metric", http.StatusBadRequest)
-	}
-
-	switch typeMetric {
-	case "counter":
-		err := s.UpdateCounter(nameMetric, valueMetric)
-		if err != nil {
-			http.Error(w, "Invalid value metric", http.StatusBadRequest)
-		}
-	case "gauge":
-		err := s.UpdateGauge(nameMetric, valueMetric)
-		if err != nil {
-			http.Error(w, "Invalid value metric", http.StatusBadRequest)
-		}
-	default:
-		http.Error(w, "Invalid type metric", http.StatusBadRequest)
-	}
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-
-}
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", mainPage)
-	mux.HandleFunc("/update/", update)
+	mux.HandleFunc("/update/", server.Update)
 
 	fmt.Println(http.ListenAndServe(":8080", mux))
 }
