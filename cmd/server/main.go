c15c40db3dec915951257d27906378fb47f9d373
Morzisorn <morzisorc1@gmail.com>
2025-01-28 16:29:40 +0000

    Fix response error codes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,13 +7,8 @@ import (
 	server "github.com/morzisorn/metrics/internal/server/handlers"
 )
 
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
-}
-
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", mainPage)
 	mux.HandleFunc("/update/", server.Update)
 
 	fmt.Println(http.ListenAndServe(":8080", mux))
