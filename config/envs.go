741e8b449be42d7ff7339212ba034963ac7dbdfb
Morzisorn <morzisorc1@gmail.com>
2025-02-24 15:27:29 +0000

    Iter9: first try

---
+++ config/envs.go
@@ -0,0 +1,99 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+func loadEnvFile(envPath string) error {
+	return godotenv.Load(envPath)
+}
+
+func (c *Config) parseEnv(app string) error {
+	switch app {
+	case "agent":
+		c.parseAgentEnvs()
+	case "server":
+		c.parseServerEnvs()
+	}
+
+	return nil
+}
+
+func (c *Config) parseAgentEnvs() {
+	c.parseAgentFlags()
+
+	addr := os.Getenv("ADDRESS")
+	if addr != "" {
+		c.Addr = addr
+	}
+
+	f, err := getEnvFloat("POLL_INTERVAL")
+	if err == nil && f != 0 {
+		c.PollInterval = f
+	}
+
+	f, err = getEnvFloat("REPORT_INTERVAL")
+	if err == nil && f != 0 {
+		c.ReportInterval = f
+	}
+}
+
+func (c *Config) parseServerEnvs() {
+	c.parseServerFlags()
+
+	addr := os.Getenv("ADDRESS")
+	if addr != "" {
+		c.Addr = addr
+	}
+
+	i, err := getEnvInt("STORE_INTERVAL")
+	if err == nil {
+		c.StoreInterval = i
+	}
+
+	s, err := getEnvString("FILE_STORAGE_PATH")
+	if err == nil && s != "" {
+		c.FileStoragePath = s
+	}
+
+	b, err := getEnvBool("RESTORE")
+	if err == nil {
+		c.Restore = b
+	}
+}
+
+func getEnvFloat(key string) (float64, error) {
+	env := os.Getenv(key)
+	if env != "" {
+		return strconv.ParseFloat(env, 64)
+	}
+	return 0, fmt.Errorf("env %s not found", key)
+}
+
+func getEnvInt(key string) (int64, error) {
+	env := os.Getenv(key)
+	if env != "" {
+		return strconv.ParseInt(env, 10, 64)
+	}
+	return 0, fmt.Errorf("env %s not found", key)
+}
+
+func getEnvString(key string) (string, error) {
+	env := os.Getenv(key)
+	if env != "" {
+		return env, nil
+	}
+	return "", fmt.Errorf("env %s not found", key)
+}
+
+func getEnvBool(key string) (bool, error) {
+	env := os.Getenv(key)
+	if env != "" {
+		return strconv.ParseBool(env)
+	}
+	return false, fmt.Errorf("env %s not found", key)
+}
---
+++ internal/server/storage/file_storage.go
@@ -0,0 +1,147 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+
+	"github.com/morzisorn/metrics/config"
+	"github.com/morzisorn/metrics/internal/server/logger"
+	"go.uber.org/zap"
+)
+
+type FileStorage struct {
+	Producer *FileStorageProducer
+	Consumer *FileStorageConsumer
+}
+
+type FileStorageProducer struct {
+	filename string
+	file     *os.File
+	encoder  *json.Encoder
+}
+
+type FileMetric struct {
+	Name  string  `json:"name"`
+	Value float64 `json:"value"`
+}
+
+var (
+	fileInstance *FileStorage
+	onceFile     sync.Once
+)
+
+func GetFileStorage() *FileStorage {
+	onceFile.Do(func() {
+		var err error
+		service := config.GetService("server")
+		fileInstance, err = NewFileStorage(service.Config.FileStoragePath)
+		if err != nil {
+			logger.Log.Panic("Error file loading storage", zap.Error(err))
+		}
+	})
+	return fileInstance
+}
+
+func NewFileStorageProducer(filename string) (*FileStorageProducer, error) {
+	return &FileStorageProducer{
+		filename: filename,
+	}, nil
+}
+
+type FileStorageConsumer struct {
+	file    *os.File
+	decoder *json.Decoder
+}
+
+func NewFileStorageConsumer(filename string) (*FileStorageConsumer, error) {
+	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &FileStorageConsumer{
+		file:    file,
+		decoder: json.NewDecoder(file),
+	}, nil
+}
+
+func NewFileStorage(filepath string) (*FileStorage, error) {
+	producer, err := NewFileStorageProducer(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	consumer, err := NewFileStorageConsumer(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileStorage{
+		Producer: producer,
+		Consumer: consumer,
+	}, nil
+}
+
+func (p *FileStorageProducer) WriteMetric(name string, value float64) error {
+	var err error
+	p.file, err = os.OpenFile(p.filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer p.file.Close()
+
+	p.encoder = json.NewEncoder(p.file)
+
+	metric := FileMetric{
+		Name:  name,
+		Value: value,
+	}
+	return p.encoder.Encode(metric)
+}
+
+func (c *FileStorageConsumer) ReadMetric() (*FileMetric, error) {
+	var metric FileMetric
+	err := c.decoder.Decode(&metric)
+	if err != nil {
+		return nil, err
+	}
+	return &metric, nil
+}
+
+func (c *FileStorageConsumer) Close() error {
+	return c.file.Close()
+}
+
+func (p *FileStorageProducer) Close() error {
+	return p.file.Close()
+}
+
+func (f *FileStorage) Close() error {
+	err := f.Producer.Close()
+	if err != nil {
+		return err
+	}
+	return f.Consumer.Close()
+}
+
+func (f *FileStorage) WriteMetrics(metrics *map[string]float64) error {
+	for name, value := range *metrics {
+		err := f.Producer.WriteMetric(name, value)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (f *FileStorage) ReadMetrics() (*map[string]float64, error) {
+	var metrics = make(map[string]float64)
+	for {
+		metric, err := f.Consumer.ReadMetric()
+		if err != nil {
+			break
+		}
+		metrics[metric.Name] = metric.Value
+	}
+	return &metrics, nil
+}
