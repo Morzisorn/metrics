da244df82c54ab4ba3456fdbb2eedade04f5c7da
Morzisorn <morzisorc1@gmail.com>
2025-03-08 17:37:49 +0000

    Iter12: return bulk send by agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -27,16 +27,16 @@ func RunAgent() error {
 			if time.Since(lastReport).Seconds() >= Service.Config.ReportInterval {
 				if len(m.Metrics) > 0 {
 					lastReport = time.Now()
-					err := c.SendMetricsByOne(&m)
-					if err != nil {
-						return err
-					}
 					/*
-						err = c.SendMetricsBatch(&m)
+						err := c.SendMetricsByOne(&m)
 						if err != nil {
 							return err
 						}
 					*/
+					err = c.SendMetricsBatch(&m)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
