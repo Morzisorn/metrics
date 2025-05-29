38cece93668305de9b3943c667f29b4c8a59aa30
Morzisorn <morzisorc1@gmail.com>
2025-05-29 18:20:12 +0100

    Iter20: fix review

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,11 +16,20 @@ import (
 	"github.com/morzisorn/metrics/internal/server/services/pages"
 )
 
-var (
-	buildVersion string
-	buildDate    string
-	buildCommit  string
-)
+// buildVersion represents the version of the application.
+// It is injected during build using -ldflags.
+// Example: go build -ldflags "-X main.buildVersion=1.0.0"
+var buildVersion string
+
+// buildDate represents the build date of the application.
+// It is injected during build using -ldflags.
+// Example: go build -ldflags "-X main.buildDate=2025-05-29"
+var buildDate string
+
+// buildCommit represents the commit from which the application was built.
+// It is injected during build using -ldflags.
+// Example: go build -ldflags "-X main.buildCommit=abc123"
+var buildCommit string
 
 var (
 	cnfg *config.Service
